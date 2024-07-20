0793bab4a83e1aa6cbbd93056cda27048218f520
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2024-07-20 14:47:57 +0800

    fix: Favicon Issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
       <body>
           <div className={``}>
             <div className={`$xl:max-w-full w-full`}>
